5fc1d3d09ef1aca7ca5f7b4bef4a8d56bb9c11c7
Tomás Rodríguez <torodriguezt@unal.edu.co>
2022-09-21 13:38:42 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -81,10 +81,9 @@ public class Animal {
 	
 	public String toString() {
 		if (Zona.get(0).getZoo() != null)
-			return "Mi nombre es " +  nombre + "tengo una edad de" + edad + "habito en " + habitat + "y mi genero es  " + genero +", la zona en la que me ubico es " + zonas.get(0) + "en el" + zonas.get(0).getZoo();
+			return "Mi nombre es " +  nombre + "tengo una edad de" + edad + "habito en " + habitat + "y mi genero es  " + genero +", la zona en la que me ubico es " + zona.get(0) + "en el" + zona.get(0).getZoo();
 		else
-			return
-					"Mi nombre es " +  nombre + "tengo una edad de" + edad + "habito en " + habitat + "y mi genero es  " + genero;
+			return "Mi nombre es " +  nombre + "tengo una edad de" + edad + "habito en " + habitat + "y mi genero es  " + genero;
 	
 	}
 }
