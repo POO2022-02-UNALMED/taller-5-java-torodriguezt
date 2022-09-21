8e7d19ff238bb51e2be47b633293692bb0414051
Tomás Rodríguez <torodriguezt@unal.edu.co>
2022-09-21 14:47:13 -0500

    Update Zona.java

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -6,12 +6,12 @@ import zooAnimales.Animal;
 public class Zona {
 	
 	private String nombre;
-	private Zoologico[] zoo = new Zoologico[1];
+	private Zoologico zoo;
 	private ArrayList<Animal> animales = new ArrayList<Animal>();
 	
 	public Zona(String nombre, Zoologico lugar) {
 		this.nombre = nombre;
-		zoo[0] = lugar;
+		zoo = lugar;
 	}
 	
 	public Zona() {
