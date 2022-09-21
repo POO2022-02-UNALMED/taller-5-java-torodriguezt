fc0b12142469b655830bfa2c7b8bb0acf0e95037
Tomás Rodríguez <torodriguezt@unal.edu.co>
2022-09-21 18:57:01 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -77,7 +77,7 @@ public class Animal {
 	
 	public static String totalPorTipo() {
 		
-		String mensaje = "Mamiferos:" + Mamifero.cantidadMamiferos() + "\n" + "Aves:" + Ave.cantidadAves() + "\n" + "Reptiles" + Reptil.cantidadReptiles();
+		String mensaje = "Mamiferos:" + Mamifero.cantidadMamiferos() + "\n" + "Aves:" + Ave.cantidadAves() + "\n" + "Reptiles:" + Reptil.cantidadReptiles() + "\n" + "Peces" + Pez.cantidadPeces();
 	
 		return mensaje;
 	}
